35922224078ccee2a855b28d626b9f56c3372fd5
JuneeDinh <dinhttnhung9910@gmail.com>
2021-10-14 16:43:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent='👏 Correct Number!';
+
+document.querySelector('.number').textContent='15';
+document.querySelector('.score').textContent='10';
+
+document.querySelector('.guess').value=12;
+console.log(document.querySelector('.guess').value);
