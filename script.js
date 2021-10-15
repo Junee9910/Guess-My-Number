0f42af479cb049dd4a30aae4fd2e9a8d02fd2a89
JuneeDinh <dinhttnhung9910@gmail.com>
2021-10-15 11:28:47 +0700

    game complete

--- script.js
+++ script.js
@@ -1,10 +1,57 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent='👏 Correct Number!';
+let secretNumber=Math.trunc(Math.random()*20)+1;
+// const number=Math.floor(Math.random()*21);
+let score=20;
+let highScore=0;
 
-document.querySelector('.number').textContent='15';
-document.querySelector('.score').textContent='10';
+function displayMessage(message){
+    document.querySelector('.message').textContent=message;
+}
+function setScore(score){
+    document.querySelector('.score').textContent=score;
+}
+document.querySelector('.number').textContent='?';
 
-document.querySelector('.guess').value=12;
-console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click',function(){
+    const guess=Number(document.querySelector('.guess').value);
+    console.log(guess);
+    
+    if(!guess){
+        displayMessage('💢 No Number');
+    } else if(guess===secretNumber)
+    {
+        displayMessage('👏 Correct Number!');
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        if(highScore<score)
+        {
+            highScore=score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+    } else if (guess!==secretNumber)
+    {
+        if (score>1)
+        {
+            displayMessage(guess>secretNumber?'↗ Too high!':'↘ Too low!');
+            score--;
+            setScore(score);
+        } else {
+            displayMessage('💥 You lost the game');
+            setScore(0);
+        }
+        
+    }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    secretNumber=Math.trunc(Math.random()*20)+1;
+    score=20;
+
+    displayMessage('Start Guesssing...');
+    document.querySelector('body').style.backgroundColor = '#222';
+    setScore(score);
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?';
+});
